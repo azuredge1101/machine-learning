83b33220694d85fdcf2d38a3834abf20efc07722
azuredge1101 <132150151+azuredge1101@users.noreply.github.com>
2023-06-04 21:50:28 +0800

    house_multiple_linear_regression.py

--- house_multiple_linear_regression.py
+++ house_multiple_linear_regression.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Downloads\house.csv')
+dataset = pd.read_csv('house.csv')
+#查詢有無缺失資料
+dataset.isna().sum()
 
 x = dataset.iloc[:, [2,3,4,5,6,7,9]].values
 y = dataset.iloc[:, [8]].values
