cd246f4f5126123db03d4bd52c84a2a39e5f3635
azuredge1101 <132150151+azuredge1101@users.noreply.github.com>
2023-06-11 20:39:38 +0800

    Update wine_logistic_linear_regression.py

--- wine_logistic_linear_regression.py
+++ wine_logistic_linear_regression.py
@@ -1,38 +1,43 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-data = pd.read_csv('wine.csv')
+dataset = pd.read_csv('wine.csv')
+
 #查詢有無缺失資料
 dataset.isna().sum()
 
-x = data.loc[:, ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']].values
-y = data.loc[:, ['quality']].values
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 11].values
 
 # 補齊缺漏數值
 from sklearn.impute import SimpleImputer
 
-imputer = SimpleImputer(missing_values=np.nan, strategy="mean", fill_value=None)
-imputer = imputer.fit(x[:,:])
-x[:,:] = imputer.transform(x[:,:])
-
-imputer2 = SimpleImputer(missing_values=np.nan, strategy="most_frequent", fill_value=None)
-imputer2 = imputer.fit(y[:,:])
-y[:,:] = imputer2.transform(y[:,:])
-
+imputer =SimpleImputer(missing_values=np.nan,strategy="most_frequent",fill_value=None)
+y=np.reshape(y,(-1, 1))
+imputer = imputer.fit(y[:,0:1])
+y[:,0:1] = imputer.transform(y[:,0:1])
+imputer =SimpleImputer(missing_values=np.nan,strategy="mean",fill_value=None)
+imputer = imputer.fit(X[:,0:11])
+X[:,0:11] = imputer.transform(X[:,0:11])
 
 # 建立訓練測試集合
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=0)
 
-# 建立邏輯迴歸模型
-from sklearn.linear_model import LogisticRegression 
-classifier= LogisticRegression(multi_class='auto', solver='newton-cg',random_state=0)
-classifier.fit(x_train, y_train.astype('int')) # 訓練模型
+#特徵縮放
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train) 
+X_test = sc_X.transform(X_test)
+#建立邏輯回歸模型
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
 
-# 測試集預測值
-y_pred = classifier.predict(x_test)
-
-# confusion matrix
+y_pred = classifier.predict(X_test)
+#比對y_pred和y_test的資料的差異
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+
