ce299bf95b9d0ac96c7d6eb201e11f7472806ba1
azuredge1101 <132150151+azuredge1101@users.noreply.github.com>
2023-06-12 13:45:41 +0800

    Update insurance多元線性回歸.py

--- insurance多元線性回歸.py
+++ insurance多元線性回歸.py
@@ -48,8 +48,7 @@ X=X[:,1:]
 
 #分割資料
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,
-random_state=0)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
